6865f42799e815500eb6d6b78a2c2d67c5c4d3fd
amolpol_98 <amolpol98@gmail.com>
2017-12-25 18:05:01 +0530

    showing 404 errors

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,21 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Author, Tag, Category, Post
 
 def index(request):
     return HttpResponse('hello django')
 
 def post_list(request):
-    posts = Post.objects.all()
+    posts = Post.objects.order_by('-id').all()
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-def post_detail(request, pk):
-    post = Post.objects.get(pk=pk)
+def post_detail(request, pk, post_slug):
+    post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_by_category(request, category_slug):
-    category = Category.objects.get(slug=category_slug)
-    posts = Post.objects.filter(category__slug=category_slug)
+    category = get_object_or_404(Category, slug=category_slug)
+    posts = get_list_or_404(Post.objects.order_by('-id'), category=category)
     context = {
         'category': category,
         'posts': posts
@@ -23,10 +23,19 @@ def post_by_category(request, category_slug):
     return render(request, 'blog/post_by_category.html', context)
 
 def post_by_tag(request, tag_slug):
-    tag = Tag.objects.get(slug=tag_slug)
-    posts = Post.objects.filter(tags__name=tag)
+    tag = get_object_or_404(Tag, slug=tag_slug)
+    posts = get_list_or_404(Post.objects.order_by('-id'), tags=tag)
     context = {
         'tag': tag,
         'posts': posts
     }
     return render(request, 'blog/post_by_tag.html', context )
+
+def post_by_author(request, name):
+    author = get_object_or_404(Author, name=name)
+    posts = get_list_or_404(Post.objects.order_by('-id'), author=author)
+    context = {
+        'author': author,
+        'posts': posts
+    }
+    return render(request, 'blog/post_by_author.html', context )
